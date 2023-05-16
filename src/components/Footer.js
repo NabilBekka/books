7a25aa6f6f0d9167cfc07e588ab3283c410db741
Nabil <bekkanabil06@gmail.com>
2023-05-16 21:56:59 +0200

    Pret au deploiement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>Projet réalisé par NabilBekka en 2023, en s'inspirant de la formation <a href="https://www.udemy.com/course/formation-react-js-pour-tous/" target="_blanc">React JS pour Tous</a> (partie Redux).</p>
-      <p>Pour la recherche des livres sur Google Books, j'utilise <a href="https://developers.google.com/books/docs/v1/using?hl=fr" target="_blanc">GOOGLE BOOKS API</a>.</p>
+      <p>Utilisation de <a href="https://developers.google.com/books/docs/v1/using?hl=fr" target="_blanc">GOOGLE BOOKS API</a> pour la recherche des livres sur Google Books.</p>
     </footer>
   )
 }
